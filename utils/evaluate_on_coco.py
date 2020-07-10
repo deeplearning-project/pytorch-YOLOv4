75b27f4df2a1f21d16ce4a87c00ac09e6d948838
1170300521 <hitzhangyabo2017@163.com>
2020-07-10 01:28:03 +0000

    complete eval scripts

--- utils/evaluate_on_coco.py
+++ utils/evaluate_on_coco.py
@@ -165,7 +165,7 @@ def test(model, annotations, cfg):
         print("Annotations do not have 'images' key")
         return
     images = annotations["images"]
-    images = images[:1000]
+    #images = images[:1000]
     resFile = 'data/coco_val_outputs.json'
 
     if torch.cuda.is_available():
